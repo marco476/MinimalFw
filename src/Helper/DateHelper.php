568268452c7909b98174fed17a85984f33a9bc62
marco <marco@marco-K56CM.lan>
2017-02-20 22:05:34 +0100

    Error description update. Remove php7 sintax

--- src/Helper/DateHelper.php
+++ src/Helper/DateHelper.php
@@ -3,6 +3,7 @@ namespace Helper;
 
 class DateHelper
 {
+    //Check if $date is in the future.
     public static function isDateInFuture(\DateTime $date)
     {
         return $date->getTimestamp() > date_create()->getTimestamp();
--- src/Helper/ErrorHelper.php
+++ src/Helper/ErrorHelper.php
@@ -6,10 +6,10 @@ class ErrorHelper
     const WARNING = E_USER_WARNING;
     const FATAL = E_USER_ERROR;
 
-    const EMPTY_OR_ERROR_ROUTER = 'Non è stata settata alcuna rotta, o non sono stati settati i parametri obbligatori per ciascuna di essa.';
+    const EMPTY_OR_ERROR_ROUTER = "No all routes are created or not necessary parameters was created for everyone";
     const DIR_VIEWS_NOT_CREATE = "I can't create the directory Views in /src/Views. Why don't you try to create manually?";
 
-    public static function setError(string $error, $levelError)
+    public static function setError($error, $levelError)
     {
         return trigger_error($error, $levelError);
     }
